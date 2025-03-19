54fbfcd11ac641ddc32deccf976c0778e10d994d
nitin <nitingupta10a@gmail.com>
2025-03-19 20:45:08 +0530

    US STATES NAME GUESSING GAME

---
+++ US STATES NAME GUESSING GAME/main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas
+
+s = turtle.Screen()
+s.title("U.S. States Game")
+image = "./blank_states_img.gif"
+s.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("./50_states.csv")
+
+def get_state_name():
+    return s.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    name = get_state_name()
+    if name is None:
+        break
+
+    if name in guessed_states:
+        print("State already guessed.")
+    elif name in data["state"].values:
+        guessed_states.append(name)
+
+        state_details = data[data["state"] == name]
+        x = int(state_details["x"])
+        y = int(state_details["y"])
+
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        t.goto(x, y)
+        t.write(name, align="center", font=("Arial", 12, "normal"))
+    else:
+        print("Invalid state name.")
+
+s.exitonclick()
